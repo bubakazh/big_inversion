752258cbddc21384991b3d489a6e5fa594264b5d
bubakazh <troymanansala@gmail.com>
2022-11-01 14:09:20 -0700

    smol deltas

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,48 @@ import './App.css';
 
 function App() {
 
-  // const user = {
-  //   firstName : "Angelica",
-  //   lastName : "Cruz",
-  //   age : 24,
-  //   hairColor: "black, dyed brown"
-  // }
+  // ! TODO INSTEAD OF MAKING INDIVIDUAL USERS, MAKE AN ARRAY OF OBJECTS, AND ITERATE THROUGH THE ARRAY WITH map function 
+
+  const user = {
+    firstName : "Angelica",
+    lastName : "Cruz",
+    age : 24,
+    hairColor: "black, dyed brown"
+  }
+
+  const user2 = {
+    firstName : "ひたぎ",
+    lastName : "戦場ヶ原",
+    age : 18,
+    hairColor: "黒"
+  }
+
+  const user3 = {
+    firstName : "Аіда",
+    lastName : "Ніколайчук",
+    age : 40,
+    hairColor: "blonde"
+  }
+
+  const user4 = {
+    firstName : "Troy",
+    lastName : "Manansala",
+    age : 24,
+    hairColor: "black, partially dyed blonde"
+  }
 
   return (
     <fieldset>
       <legend>App.jsx</legend>
-      <PersonCard firstName = "Troy" lastName = "Manansala" age = {33} hairColor = "black, partially dyed blonde" />
-      <PersonCard firstName = "Angelica" lastName = "Cruz" age = {24} hairColor = "black, partially dyed blonde" />
+      {/* <PersonCard firstName = "Troy" lastName = "Manansala" age = {33} hairColor = "black, partially dyed blonde" />
+      <PersonCard firstName = "Angelica" lastName = "Cruz" age = {24} hairColor = "black, partially dyed brown" />
       <PersonCard firstName = "ひたぎ" lastName = "戦場ヶ原" age = {18} hairColor = "黒" />
-      <PersonCard firstName = "Аіда" lastName = "Ніколайчук" age = {40} hairColor = "blonde" />
-      {/* <PersonCard user = {user}/> */}
+      <PersonCard firstName = "Аіда" lastName = "Ніколайчук" age = {40} hairColor = "blonde" /> */}
+      <PersonCard user = {user}/>
+      <PersonCard user = {user2}/>
+      <PersonCard user = {user3}/>
+      <PersonCard user = {user4}/>
+
     </fieldset>
   );
 }
--- src/PersonCard.jsx
+++ src/PersonCard.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 
-export default function PersonCard (props) {
-  return (
-    <fieldset>
-        <legend>PersonCard.jsx</legend>
-        <h1> {props.lastName}, {props.firstName}</h1>
-        <p>Age: {props.age}</p>
-        <p>Hair color: {props.hairColor}</p>
-    </fieldset>
-  )
-}
+// export default function PersonCard (props) {
+//   return (
+//     <fieldset>
+//         <legend>PersonCard.jsx</legend>
+//         <h1> {props.lastName}, {props.firstName}</h1>
+//         <p>Age: {props.age}</p>
+//         <p>Hair color: {props.hairColor}</p>
+//     </fieldset>
+//   )
+// }
 
-// export default function PersonCard (user) {
-//     const {firstName, lastName, age, hairColor} = user;
-//     console.log(user);
-//     console.log(this.props.user.lastName);
-//         return (
-//         <fieldset>
-//             <legend>PersonCard.jsx</legend>
-//             <h1> {lastName}, {firstName}</h1>
-//             <p>Age: {age}</p>
-//             <p>Hair color: {hairColor}</p>
-//         </fieldset>
-//     )
-//   }
+export default function PersonCard ({user}) {
+    // ! THE PersonCard FUNCTION'S PARAMETER user, NEEDS TO BE IN {}, BECAUSE IT'S A DICTIONARY, NOT JUST A STRING.
+    
+    const {firstName, lastName, age, hairColor} = user;
+    // console.log(user);
+    // console.log(lastName);
+        return (
+        <fieldset>
+            <legend>PersonCard.jsx</legend>
+            <h1> {lastName}, {firstName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair color: {hairColor}</p>
+        </fieldset>
+    )
+  }
